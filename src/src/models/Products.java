3ba54f0384b4bd966111906f4fdb512057227fbe
maroskayoussef <maroskayoussef1@gmail.com>
2025-07-05 21:37:25 +0300

    Create Products.java

---
+++ src/src/models/Products.java
@@ -0,0 +1,24 @@
+abstract class Products{
+    String name;
+    double price;
+    int quantity;
+    
+    public Products (String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+ 
+}
